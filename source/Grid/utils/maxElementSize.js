abfab3c8f9518a13f82a0c8586f3d50fb6a4faed
David Terins <david.terins@telia.com>
2020-11-16 00:24:46 +0100

    Changed max size to Chrome.

--- source/Grid/utils/maxElementSize.js
+++ source/Grid/utils/maxElementSize.js
@@ -7,11 +7,12 @@ const isBrowser = () => typeof window !== 'undefined';
 
 const isChrome = () => !!window.chrome;
 
-export const getMaxElementSize = (): number => {
-  if (isBrowser()) {
-    if (isChrome()) {
-      return CHROME_MAX_ELEMENT_SIZE;
-    }
-  }
-  return DEFAULT_MAX_ELEMENT_SIZE;
-};
+export const getMaxElementSize = (): number => CHROME_MAX_ELEMENT_SIZE;
+//  {
+//   if (isBrowser()) {
+//     if (isChrome()) {
+//       return CHROME_MAX_ELEMENT_SIZE;
+//     }
+//   }
+//   return DEFAULT_MAX_ELEMENT_SIZE;
+// };
